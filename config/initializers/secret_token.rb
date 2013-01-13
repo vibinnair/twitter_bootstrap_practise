72a69b0e493c923216e289b37b1dfd6fbcb840dd
vibinnair <vibin.nair@gmail.com>
2013-01-14 01:06:14 +0530

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterBootstrapPractise::Application.config.secret_token = 'a537c0ad0db6d8374a5736e96e00a7be1859480d591cadab61a60f7b8d9e27f061af4604625233406bec135a9a78e7e4ccb00bcf0aa003093ba7f9d446fcff5a'
